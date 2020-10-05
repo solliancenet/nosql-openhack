08efe53ac1f225b22b2b3e57fc58cb1bd8afcd72
Kyle Burns <31851557+kyleburnsdev@users.noreply.github.com>
2020-10-05 14:41:37 -0400

    Adds website availability check
    adds availability check for deployed website to act as a smoke test for the deployment

--- deploy.ps1
+++ deploy.ps1
@@ -56,3 +56,14 @@ while ($importStatus.Status -eq "InProgress") {
 
 [Console]::WriteLine("")
 $importStatus
+
+$webUrl = "https://openhackweb-" + $suffix + ".azurewebsites.net"
+[Console]::WriteLine("Checking Website Availability")
+$availabilityResult = Invoke-WebRequest $webUrl
+
+if($availabilityResult.StatusCode -eq 200) {
+    Write-Output ("Website is available")
+}
+else {
+    Write-Output("Website availability check failed for team: " + $teamName)
+}
